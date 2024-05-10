b900f084b22c9129fc40aef282dd28c1d0cbee06
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2024-05-10 15:42:57 -0400

    Gradient color added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       backgroundImage: {
         "gradient-linear":
           "linear-gradient(222.94deg, #051A91 -4.31%, #061C93 14.41%, #2284F1 81.88%, #1F80EB 103.81%)",
+        "linear": "linear-gradient(90deg, #1F80EB, #2284F1, #061C93, #051A91)",
       },
     },
   },
